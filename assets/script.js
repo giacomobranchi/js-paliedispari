8653cedf6a1f07d0fe91a2c2414c45cc7d7c0058
giacomobranchi <branchigiacomo28@gmail.com>
2023-08-31 16:03:50 +0200

    refactoring per maggiore leggibilità

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,23 @@ let oddEven = Number(prompt("Inserisci 0 per pari o 1 per dispari"));
 let userNum = Number(prompt("Inserisci un numero da 1 a 5"));
 
 
-console.log(oddEven, userNum);
+
 
 function pcNumber() {
     return Math.floor(Math.random() * 5 + 1);
 }
 
-console.log(pcNumber());
+
 
 function checker(user, pc) {
-    return ((user + pc) % 2) ? false : true //Il return da False se la somma è dispari e True se è pari
+    return (user + pc) % 2 //Il return da False se la somma è dispari e True se è pari
 }
 
-console.log(checker(1, 4));
+
 
 let check = checker(userNum, pcNumber())
 
-if (check != oddEven) { //se il valore booleano è diverso significa che sono uguali
+if (check == oddEven) { //check se sono uguali
     alert("Hai vinto!")
 } else {
     alert("Hai perso!")
